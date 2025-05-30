d61c8aa486d912c1a07f49da6385547e93434450
EverthKantun <everth.ko@gmail.com>
2025-05-30 12:58:26 -0600

    All entities created

---
+++ src/main/java/com/tecdesoftware/market/persistence/entity/Cliente.java
@@ -0,0 +1,72 @@
+package com.tecdesoftware.market.persistence.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Cliente {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Long getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Long celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    @Id
+    //no usamos generatedValue porque no lo vamos a generar automático, será la CURP
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+
+    private Long celular;
+    private String direccion;
+    @Column(name="correo_electronico")
+    private String correoElectronico;
+
+
+}
---
+++ src/main/java/com/tecdesoftware/market/persistence/entity/CompraProductoPK.java
@@ -0,0 +1,35 @@
+package com.tecdesoftware.market.persistence.entity;
+//para unir las dos llaves y crear una llave compuesta
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Embeddable
+//serializable es para que sea autoincrementable
+public class CompraProductoPK implements Serializable {
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    @Column(name = "id_compra")
+    private int idCompra;
+
+    @Column(name="id_producto")
+    private int idProducto;
+
+}
